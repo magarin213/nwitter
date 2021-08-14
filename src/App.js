7a69f7ed6baaab7522bd0e17a2cd6dab6c73b9d4
magarin <mario213@nate.com>
2021-08-14 14:10:07 +0900

    react, firebase install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import react from 'react';
 
 function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwwxnTsqAVRCBpL7DiLvxB38BpkGNOk1o",
+  authDomain: "nwitter-51e20.firebaseapp.com",
+  projectId: "nwitter-51e20",
+  storageBucket: "nwitter-51e20.appspot.com",
+  messagingSenderId: "571621407983",
+  appId: "1:571621407983:web:74de7e696bee4e9ae26f1a"
+};
+
+export default firebase.initializeApp(firebaseConfig);
