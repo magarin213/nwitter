e3fac39f2c63e1f686ff6e47245af7e5331a3b07
magarin <mario213@nate.com>
2021-08-16 20:43:10 +0900

    db 구축, 게시글 실시간 업데이트

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,9 @@ import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Navigation from 'components/Navigation';
 import Profile from 'routes/Profile';
+import React from "react";
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({isLoggedIn, userObj}) => {
 
   return (
     <Router>
@@ -13,7 +14,7 @@ const AppRouter = ({isLoggedIn}) => {
         {isLoggedIn ? (
           <>
             <Route exact path="/">
-              <Home />
+              <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile />
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import { dbService } from 'fbase';
+import React, {useState, useEffect} from 'react'
 
-function Home() {
+function Home({userObj}) {
+
+  const [nweet, setNweet] = useState("")
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+
+    dbService.collection("nweets").onSnapshot(snapshot => {
+      const nweetArray = snapshot.docs.map(doc => ({
+        id:doc.id, ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    })
+  }, []);
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+   await dbService.collection("nweets").add({
+      text : nweet,
+      createAt : Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  }
+
+  const onChang = (e) => {
+    const { target : {value} ,
+  } = e;
+  setNweet(value)
+  }
+
   return (
-    <span>
-      Home
-    </span>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChang} type="text" placeholder="What's on your mind?" maxLength={120}/>
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map(nweet => 
+          <div key={nweet.id}>
+          <h4>{nweet.text}</h4>
+          </div>)}
+      </div>
+    </div>
   )
 }
 
